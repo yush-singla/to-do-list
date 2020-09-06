34d332fdca88df19273e8a5f419e942bf43dcf64
yushsingla <yushsingla@gmail.com>
2020-09-06 15:49:41 +0530

    added ports and procfile and gitignore

--- app.js
+++ app.js
@@ -119,6 +119,6 @@ app.get("/:parameter",function(req,res){
   });
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(process.env.PORT||3000, function() {
+  console.log("Server started now");
 });
